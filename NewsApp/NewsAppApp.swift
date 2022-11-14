8d5a17f030529148379ba8c548ebf226c5a565fe
Agus Adi <gusadi872@gmail.com>
2022-11-14 19:56:57 +0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Gus Adi on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
